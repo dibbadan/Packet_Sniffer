169054d6f15176f74038cc6cdc9664216e7deaf3
dibbadan <daniele.dibattista@studenti.polito.it>
2022-08-02 12:20:30 +0200

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,14 @@ mod parser;
 mod sniffer;
 
 fn main() {
-<<<<<<< HEAD
     let mut sniffer = Sniffer::new();
-=======
-    //push
-    let cli = Cli::parse();
 
-
-    
-
-    
-    let _passed_args = Cli::show_passed_args(&cli);
+    //let cli = Cli::parse();
+    //let _passed_args = Cli::show_passed_args(&cli);
     //println!("PASSED ARGUMENTS = {:?}", passed_args);
 
     //let mut sniffer = Sniffer::new();
 
-    /*
->>>>>>> 03f9c4b4cc5a283b2c23e335d80e28424289f829
     let _devices = sniffer.list_devices();
     sniffer.sniff(_devices[0].clone());
     sniffer.show_map();
